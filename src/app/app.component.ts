3e0444b03ae8f6e45ecca791ccd1ac3600b2afbb
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-03-06 12:04:38 +0530

    [Ayushmaan Tiwari]Add.Ensure the userName is the valid input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,25 @@ export class AppComponent {
   ngOnInit(){
     this.title = 'Hello from Bridgelabz';
   }
+
   image ="BL-logo.jpg";
   url = "https://www.bridgelabz.com";
+
   onClick($event: any){
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
 }
 userName: string = "";
+
+nameError: string = "";
+
+onInput($event: any){
+  console.log($event);
+  const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+  if(nameRegex.test(this.userName)){
+    this.nameError = "";
+    return;
+  }
+  this.nameError = "Name is Incorrect!";
+}
 }
